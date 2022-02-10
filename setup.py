431070d0e9b65fe8018a22b995f781fe7cfb9959
Luca Fabbri <l.fabbri@bopen.eu>
2022-02-10 09:05:24 +0100

    Back to development: 0.2.2

--- setup.py
+++ setup.py
@@ -50,6 +50,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/bopen/haunts",
-    version="0.2.1",
+    version="0.2.2.dev0",
     zip_safe=False,
 )

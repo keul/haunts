6d15ab615340600b2d2cc73f2e5f7a1b11e848a4
Luca Fabbri <l.fabbri@bopen.eu>
2022-05-01 14:52:54 +0200

    Removed test code

--- haunts/calendars.py
+++ haunts/calendars.py
@@ -119,34 +119,3 @@ def create_event(config_dir, calendar, date, summary, details, length, from_time
         "link": event["htmlLink"],
     }
     return event_data
-
-
-def execute(config_dir):
-    """Shows basic usage of the Google Calendar API.
-    Prints the start and name of the next 10 events on the user's calendar.
-    """
-    get_credentials(config_dir)
-
-    service = build("calendar", "v3", credentials=creds)
-
-    # Call the Calendar API
-    now = datetime.datetime.utcnow().isoformat() + "Z"  # 'Z' indicates UTC time
-    print("Getting the upcoming 10 events")
-    events_result = (
-        service.events()
-        .list(
-            calendarId="c_bu7esjsc8qt8vc6gtjruuq94js@group.calendar.google.com",
-            timeMin=now,
-            maxResults=10,
-            singleEvents=True,
-            orderBy="startTime",
-        )
-        .execute()
-    )
-    events = events_result.get("items", [])
-
-    if not events:
-        print("No upcoming events found.")
-    for event in events:
-        start = event["start"].get("dateTime", event["start"].get("date"))
-        print(start, event["summary"])
